c814681624d4f3298cc4a56c33059c64e247be0b
rajezh130195 <rajesh130195@gmail.com>
2025-01-24 09:05:30 +0530

    code movement
    code change commit

--- step-definitions/addProductStepDef.js
+++ step-definitions/addProductStepDef.js
@@ -27,5 +27,9 @@ Then(/^the products should appear in the cart with correct details$/, async () =
     await accountInfoPage.verifyCartProducts(2);
     await accountInfoPage.verifyCartProducts(3);
     await accountInfoPage.actualExpectedValidation( $('//ul[@id="address_delivery"]/li[8]'), jsonData.userRegistration.phoneNumber);
+    await accountInfoPage.actualExpectedValidation( $('//ul[@id="address_delivery"]/li[6]'), jsonData.userRegistration.city+" "+jsonData.userRegistration.state+" "+jsonData.userRegistration.zipCode);
     console.log("User's Delivery Address Phone Number is displayed as expected");
+    await $('//textarea[@name="message"]').click();
+    await $('//textarea[@name="message"]').setValue("Placing order for all the added products in the cart for the respective user address");
+    await $('//a[text()="Place Order"]').click();
 });
